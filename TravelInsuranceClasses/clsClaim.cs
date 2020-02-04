8ac5243931a1911c28037c086721d0038a3d2e27
George Kars <p2424630@my365.ac.uk>
2020-02-04 16:28:02 +0000

    Addd Test Cases

--- TravelInsuranceClasses/clsClaim.cs
+++ TravelInsuranceClasses/clsClaim.cs
@@ -1,6 +1,14 @@
-﻿namespace TravelInsuranceTest
+﻿using System;
+
+namespace TravelInsuranceTest
 {
     public class clsClaim
     {
+        public DateTime ClaimDate { get; set; }
+        public decimal ClaimAmnt { get; set; }
+        public bool ClaimStatus { get; set; }
+        public int PolicyID { get; set; }
+        public int ClaimID { get; set; }
+        public string ClaimReason { get; set; }
     }
 }
