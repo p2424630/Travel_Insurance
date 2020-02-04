7235b50595d7421882347590f572f4f2ca463163
Lampros Karseras <P242629@my365.dmu.ac.uk>
2020-02-04 16:28:45 +0000

    Added more tests and properties in clsPolicy.

--- TravelInsuranceClasses/clsPolicy.cs
+++ TravelInsuranceClasses/clsPolicy.cs
@@ -1,6 +1,14 @@
-﻿namespace TravelInsuranceClasses
+﻿using System;
+
+namespace TravelInsuranceClasses
 {
     public class clsPolicy
     {
+        public bool Accepted { get; set; }
+        public DateTime PolicyBuyDate { get; set; }
+        public int PolicyID { get; set; }
+        public int QuoteID { get; set; }
+        public int CustomerID { get; set; }
+        public string PolicyDetails { get; set; }
     }
 }
--- TravelInsuranceTest/tstPolicy.cs
+++ TravelInsuranceTest/tstPolicy.cs
@@ -15,5 +15,82 @@ namespace TravelInsuranceTest
             //test if exists
             Assert.IsNotNull(APolicy);
         }
+
+        [TestMethod]
+        public void AcceptedPropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            Boolean TestData = true;
+            //assign the data to propery
+            APolicy.Accepted = TestData;
+            //test equality
+            Assert.AreEqual(APolicy.Accepted, TestData);
+
+        }
+
+        [TestMethod]
+        public void PolicyBuyDatePropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            DateTime TestData = DateTime.Now.Date;
+            //assign to the property
+            APolicy.PolicyBuyDate = TestData;
+            //test for equality
+            Assert.AreEqual(APolicy.PolicyBuyDate, TestData);
+        }
+        [TestMethod]
+        public void PolicyIDPropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            Int32 TestData = 1;
+            //assign to the property
+            APolicy.PolicyID = TestData;
+            //test for equality
+            Assert.AreEqual(APolicy.PolicyID, TestData);
+        }
+        [TestMethod]
+        public void QuoteIDPropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            Int32 TestData = 1;
+            //assign to the property
+            APolicy.QuoteID = TestData;
+            //test for equality
+            Assert.AreEqual(APolicy.QuoteID, TestData);
+        }
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            Int32 TestData = 1;
+            //assign to the property
+            APolicy.CustomerID = TestData;
+            //test for equality
+            Assert.AreEqual(APolicy.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void PolicyDetailsPropertyOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test data
+            string TestData = "Some details";
+            //assign to the property
+            APolicy.PolicyDetails = TestData;
+            //test for equality
+            Assert.AreEqual(APolicy.PolicyDetails, TestData);
+        }
+
     }
 }
