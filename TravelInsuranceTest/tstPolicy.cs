3d41df2469cb3660116ed44ce0a87c148ab99fa3
Lampros Karseras <P242629@my365.dmu.ac.uk>
2020-02-04 15:56:11 +0000

    Added a test for instantiating clsPolicy and checking for null.

---
+++ TravelInsuranceTest/tstPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelInsuranceClasses;
+
+namespace TravelInsuranceTest
+{
+    [TestClass]
+    public class tstPolicy
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Instantiate The class
+            clsPolicy APolicy = new clsPolicy();
+            //test if exists
+            Assert.IsNotNull(APolicy);
+        }
+    }
+}
