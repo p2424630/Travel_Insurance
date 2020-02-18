a3ab7610b8b1fe2b0be928abbc830cc26ab90e8e
EvilBritishGuy <47041462+EvilBritishGuy@users.noreply.github.com>
2020-02-18 16:47:42 +0000

    added test methods and made presentation layer - still needs stuff

---
+++ TravelInsuranceClasses/clsCustomerDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelInsuranceClasses
+{
+    public class clsCustomerDetails
+    {
+        public int setCustomerID { get; set; }
+        public string setCustomerName { get; set; }
+        public DateTime setDoB { get; set; }
+        public List<string> setAddress { get; set; }
+        public string setEmail { get; set; }
+        public bool setMarketingPreferences { get; set; }
+        public string setContactNum { get; set; }
+    }
+}
