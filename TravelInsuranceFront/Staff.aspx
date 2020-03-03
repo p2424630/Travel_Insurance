9bca7c3a8832e752e3da92b3868104e6feda4012
p2423604 <p2423604@my365.dmu.ac.uk>
2020-03-03 17:02:02 +0000

    test

---
+++ TravelInsuranceFront/Staff.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Staff.aspx.cs" Inherits="Staff" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Styles/style1.css" rel ="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="lbl_Name" runat="server" Text="Full Name"></asp:Label>
+        <input id="txt_Name" type="text" /><div>
+            <asp:Label ID="lbl_Address" runat="server" Text="Address"></asp:Label>
+            <input id="txt_Address" type="text" /></div>
+        <p>
+            <asp:Label ID="lbl_Salary" runat="server" Text="Salary"></asp:Label>
+            <input id="txt_Salary" type="text" /></p>
+        <p>
+            <asp:Label ID="lbl_PhoneNo" runat="server" Text="Phone Number"></asp:Label>
+            <input id="txt_PhoneNo" type="text" /></p>
+        <p>
+            <asp:Label ID="lbl_StartDate" runat="server" Text="Start Date"></asp:Label>
+            <input id="txt_StartDate" type="text" /></p>
+        <p>
+            <asp:Label ID="lbl_isValid" runat="server" Text="IsValid?"></asp:Label>
+            <input id="cbox_isValid" type="checkbox" /></p>
+    </form>
+</body>
+</html>
---
+++ TravelInsuranceTest/tstStaff.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelInsuranceClasses;
+
+namespace TravelInsuranceTest
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
+        }
+        [TestMethod]
+        public void NameOK()
+        {
+            clsStaff StaffName = new clsStaff();
+            string TestData = "";
+            AStaff.Name = TestData;
+            Assert.AreEqual(AStaff.Name, TestData);
+        }
+        [TestMethod]
+
+        public void SalaryOK()
+        {
+            clsStaff Amount = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.Salary = TestData;
+            Assert.AreEqual(AStaff.Salary, TestData);
+        }
+        [TestMethod]
+
+        public void PhoneNoOK()
+        {
+            clsStaff Phone = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.PhoneNo = TestData;
+            Assert.AreEqual(AStaff.PhoneNo, TestData);
+        }
+        [TestMethod]
+
+        public void StartOK()
+        {
+            clsStaff StartDate = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            AStaff.Start = TestData;
+            Assert.AreEqual(AStaff.Start, TestData);
+        }
+        [TestMethod]
+
+        public void IsValidOK()
+        {
+            clsStaff validate = new clsStaff();
+            Boolean TestData = true;
+            AStaff.IsValid = TestData;
+            Assert.AreEqual(AStaff.IsValid, TestData);
+        }
+    }
+}
